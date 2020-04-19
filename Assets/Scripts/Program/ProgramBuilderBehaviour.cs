47b3760cb41e73155342b4bfe2cc85eaf493fff4
deadalley <jessica.aissa@live.com>
2020-04-19 19:26:11 +0200

    Update arrow materials

--- Assets/Scripts/Program/ProgramBuilderBehaviour.cs
+++ Assets/Scripts/Program/ProgramBuilderBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgramBuilderBehaviour : MonoBehaviour {
     public GameObject ProgramInstructionPrefab;
@@ -14,8 +15,12 @@ public class ProgramBuilderBehaviour : MonoBehaviour {
     public void AddInstruction(IInstruction instruction) {
         int offsetY = 180;
         program.AddInstruction(instruction);
+
         GameObject programInstruction = Instantiate(ProgramInstructionPrefab, transform.position, Quaternion.identity);
         programInstruction.transform.SetParent(transform, false);
-        programInstruction.transform.localPosition = new Vector3(-702, offsetY - program.Instructions.Count * 30, 0);
+        programInstruction.transform.localPosition = new Vector3(-702, offsetY - (program.Instructions.Count - 1) * 30, 0);
+
+        Text programInstructionText = programInstruction.GetComponent<Text>();
+        programInstructionText.text = instruction.Name;
     }
 }
